ec30da46664d214eefd3506e0945ef586550adda
finka-nkvd <gila25643@gmail.com>
2024-09-27 09:12:50 +0700

    задания
    теперь 2 задания

--- 27.09.24.py
+++ 27.09.24.py
@@ -8,3 +8,24 @@ if start == 1:
         print('работает')
     else:
         print('не работает')
+elif start == 2:
+    try:
+        num1 = float(input('введивте первую циферку '))
+        num2 = float(input('введтивте вторую циферку '))
+
+    except:
+        print('циферки циферки')
+        num1 = float(input('введивте первую циферку '))
+        num2 = float(input('введтивте вторую циферку '))
+    itt = input('введитё иттерацию ')
+    if itt == '+':
+        print(num1 + num2)
+    elif itt == '-':
+        print(num1 - num2)
+    elif itt == '*':
+        print(num1 * num2)
+    elif itt == '/':
+        if num1 == 0 or num2 == 0:
+            print('ты гений на ноль делить нельзя')
+        else:
+            print(num1 / num2)
