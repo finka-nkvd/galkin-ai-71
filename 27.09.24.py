17552a55f1664df88b9d75e3f7a82633d958d722
finka-nkvd <gila25643@gmail.com>
2024-09-27 09:46:03 +0700

    задания
    теперь много

--- 27.09.24.py
+++ 27.09.24.py
@@ -32,3 +32,39 @@ elif start == 2:
             print('ты гений на ноль делить нельзя')
         else:
             print(num1 / num2)
+elif start==3:
+    num1 = float(input("введите циферку чтобы проверить на положительность"))
+    if num1 > 0:
+        print("положительно")
+    else:
+        print('отрицательно')
+elif start==4:
+    num1 = float(input('введите число чтобы проверить на четность'))
+    if num1 % 2 == 0:
+        print('четно')
+    else:
+        print("нечетно")
+elif start==5:
+    num1 = float(input('введите число чтобы проверить отрицательное положительное или 0'))
+    if num1 == 0:
+        print('ноль')
+    elif num1 < 0:
+        print('отрицательно')
+    elif num1 > 0:
+        print('положительно')
+elif start == 6:
+    num1 = int(input('введите число на проверку четности'))
+    num2 = int(input('второе'))
+    num3 = int(input('и третье'))
+    if num1 % 2 == 0 or num2 % 2 == 0 or 3 % 2 == 0:
+        print('есть четное')
+elif start == 7:
+    num = int(input('введите число чтобы проверить какое оно'))
+    if num < 10:
+        print('меньше 10')
+    elif num >= 10 and num <=20:
+        print('между 10 и 20')
+    elif num > 20:
+        print('больше 20')
+elif start == 8:
+    print('не доделано')
