37752df13058fa235176d74dbe84a8c2d720c206
finka-nkvd <gila25643@gmail.com>
2025-02-25 09:20:22 +0700

    Update classesCars.py

--- classesCars.py
+++ classesCars.py
@@ -27,28 +27,21 @@ class Car:
         print(f"Тип кузова: {self.body_type}")
         print(f"Цвет: {self.color}")
 
+    def __del__(self):
+        print(f"Машина {self.brand} фирмы {self.manufacturer} удалена")
+
+a=0
 AeroSwift = Car('AeroSwift', 'AeroTech Motors', '1.8 л (турбированный)', 'Седан', 'Перламутрово-белый')
-AeroSwift.info()
-AeroSwift.engine_start()
-AeroSwift.engine_stop()
-AeroSwift.headlights_on()
-AeroSwift.headlights_off()
-time.sleep(1)
-print('\n')
 TerraCruise = Car('TerraCruise', 'Terra Automotive Group', '3.0 л (дизель)', 'Внедорожник', 'Графитово-серый')
-TerraCruise.info()
-TerraCruise.engine_start()
-TerraCruise.engine_stop()
-TerraCruise.headlights_on()
-TerraCruise.headlights_off()
-time.sleep(1)
-print('\n')
 VoltRunner = Car('VoltRunner', 'ElectroMotion Corp.', 'Электромотор (мощность: 150 кВт)', 'Кроссовер', 'Ярко-зеленый металлик')
-VoltRunner.info()
-VoltRunner.engine_start()
-VoltRunner.engine_stop()
-VoltRunner.headlights_on()
-VoltRunner.headlights_off()
-time.sleep(1)
-print('\n')
+cars = [AeroSwift, TerraCruise, VoltRunner]
+for i in range(len(cars)):
+    cars[a].info()
+    cars[a].engine_start()
+    cars[a].engine_stop()
+    cars[a].headlights_on()
+    cars[a].headlights_off()
+    a+=1
+    time.sleep(1)
+    print('\n')
 
